25e37d1e771e66b36a81fdd4e5bb2e3d7527bcb5
theamargupta <theamargupta.tech@gmail.com>
2024-05-31 00:20:34 +0530

    fix/ag:update first set of change ag

---
+++ utils/constant.js
@@ -0,0 +1,343 @@
+export const varriable = {
+  linkedin: {
+    href: 'https://www.linkedin.com/in/amar-gupta-2684a1157/'
+  },
+  github: {
+    href: 'https://github.com/theamargupta'
+  },
+  skills: [
+    {
+      id: 'ckryhn6j4glg80c867ucy3rv3',
+      name: 'HTML5',
+      icon: 'SiHtml5',
+      color: {
+        hex: '#e34f26'
+      }
+    },
+    {
+      id: 'ckryktc3kgux70b91g7kit6m8',
+      name: 'CSS3',
+      icon: 'SiCss3',
+      color: {
+        hex: '#1572b6'
+      }
+    },
+    {
+      id: 'ckrykwcw8gtwi0b931k3oxb06',
+      name: 'JavaScript',
+      icon: 'SiJavascript',
+      color: {
+        hex: '#f7df1e'
+      }
+    },
+    {
+      id: 'ckrykwwyggv3w0b91dy3o9gax',
+      name: 'Typescript',
+      icon: 'SiTypescript',
+      color: {
+        hex: '#3178c6'
+      }
+    },
+    {
+      id: 'ckrykxbm8gu1e0b29cgknd5yk',
+      name: 'React',
+      icon: 'SiReact',
+      color: {
+        hex: '#61dafb'
+      }
+    },
+    {
+      id: 'ckrykxr1sgv5j0b91r1td4qlc',
+      name: 'Github',
+      icon: 'SiGithub',
+      color: null
+    },
+    {
+      id: 'ckrykyac8gv600b91lg03bv5m',
+      name: 'Next.js',
+      icon: 'SiNextDotJs',
+      color: null
+    },
+    {
+      id: 'ckryl8czsgutp0b29aau8epjs',
+      name: 'Firebase',
+      icon: 'SiFirebase',
+      color: {
+        hex: '#f7df1e'
+      }
+    },
+    {
+      id: 'ckrylbhncguxp0b292ppigqbh',
+      name: 'Graphql',
+      icon: 'SiGraphql',
+      color: {
+        hex: '#de33a6'
+      }
+    },
+    {
+      id: 'ckrylcy48gv5w0c86udrabt17',
+      name: 'Heroku',
+      icon: 'SiHeroku',
+      color: {
+        hex: '#430098'
+      }
+    },
+    {
+      id: 'ckryrled4h78i0b29x6sqwczq',
+      name: 'Nuxt.js',
+      icon: 'SiNuxtDotJs',
+      color: {
+        hex: '#00bd85'
+      }
+    },
+    {
+      id: 'ckryrmemoh8ds0b91f5md9ndq',
+      name: 'Vue.js',
+      icon: 'SiVueDotJs',
+      color: {
+        hex: '#3fb27f'
+      }
+    },
+    {
+      id: 'ckrza18igi4p50b93yes7tpgw',
+      name: 'Vuex',
+      icon: 'SiVueDotJs',
+      color: {
+        hex: '#3fb984'
+      }
+    },
+    {
+      id: 'ckrza278gi4q00b93pzy8w0ub',
+      name: 'Redux',
+      icon: 'SiRedux',
+      color: {
+        hex: '#764bbc'
+      }
+    },
+    {
+      id: 'ckrzah3i0i62p0c863s4slx1p',
+      name: 'Material Ui',
+      icon: 'SiMaterialUi',
+      color: {
+        hex: '#007dc5'
+      }
+    },
+    {
+      id: 'ckrzahxlci4yv0b93rv3yr97l',
+      name: 'Netlify',
+      icon: 'SiNetlify',
+      color: {
+        hex: '#008ab5'
+      }
+    },
+    {
+      id: 'ckrzx3fuwj3ui0b2983s4fvai',
+      name: 'Styled Components',
+      icon: 'SiStyledComponents',
+      color: {
+        hex: '#dd877a'
+      }
+    },
+    {
+      id: 'cks2ulx88npb40c86fgc27581',
+      name: 'Auth0',
+      icon: 'SiAuth0',
+      color: {
+        hex: '#e45123'
+      }
+    }
+  ],
+  socials: [
+    {
+      id: 'ckse9o7nkd1c70b39p7e58cmu',
+      name: 'Github',
+      color: null,
+      href: 'https://github.com/theamargupta',
+      icon: 'SiGithub'
+    },
+    {
+      id: 'ckse9ox48d1df0b39qvrvr9v7',
+      name: 'Linkedin',
+      color: '#0A66C2',
+      href: 'https://www.linkedin.com/in/amar-gupta-2684a1157/',
+      icon: 'SiLinkedin'
+    },
+    {
+      id: 'ckse9pe3cd40c0c815r0fzqen',
+      name: 'Facebook',
+      color: '#1DA1F2',
+      href: 'https://www.facebook.com/amarlalaji',
+      icon: 'SiFacebook'
+    },
+    {
+      id: 'ckse9pvu8d01f0b85f6bskx60',
+      name: 'Instagram',
+      color: '#9146FF',
+      href: 'https://www.instagram.com/amarguptta/',
+      icon: 'SiInstagram'
+    },
+    {
+      id: 'ckse9qdl4cyyl0c883ymddb82',
+      name: 'Google',
+      color: '#FF0000',
+      href: 'mailto:theamargupta.tech@gmail.com',
+      icon: 'SiGoogle'
+    }
+  ],
+  experiences: [
+    {
+      id: 'cl3bftclk2e6w0czshoxbopjz',
+      title: 'Software Engineer',
+      jobTitle: 'Frontend Developer',
+      company: 'Spark Eighteen Pvt Ltd',
+      duration: '6 mos',
+      location: 'Delhi',
+      description: 'Responsible for building Single page applications',
+      as: 'Full Time WFH',
+      date: ['2021-12-01']
+    },
+    {
+      id: 'ckrylir80gv5s0b93hvtwk22m',
+      title: 'LoudCloud Systems Pvt. Ltd.',
+      jobTitle: 'Software Engineer',
+      company: 'LoudCloud Systems',
+      duration: null,
+      location: 'Mumbai, Maharashtra, India',
+      description:
+        'Responsible for building Single page applications and websites using React/Redux and in Vue',
+      as: 'Full-time WFH',
+      date: ['2020-12-01']
+    },
+    {
+      id: 'ckseacfkgd5sz0c81ng8o8y5l',
+      title: 'Les Transformations Pvt. Ltd.',
+      jobTitle: 'Frontend Developer',
+      company: 'Les Transformations',
+      duration: '3 mos',
+      location: 'Gurugram, Haryana, India',
+      description: 'Responsible for building Single page applications',
+      as: 'Full-time WFH',
+      date: ['2020-09-01', '2020-11-30']
+    }
+  ],
+  projects: [
+    {
+      id: 'ckryl8nsogvxq0b91val16sd1',
+      title: 'Netflix',
+      tools: [
+        {
+          id: 'ckrykxbm8gu1e0b29cgknd5yk',
+          name: 'React',
+          icon: 'SiReact',
+          color: {
+            hex: '#61dafb'
+          }
+        },
+        {
+          id: 'ckrza278gi4q00b93pzy8w0ub',
+          name: 'Redux',
+          icon: 'SiRedux',
+          color: {
+            hex: '#764bbc'
+          }
+        },
+        {
+          id: 'ckrzah3i0i62p0c863s4slx1p',
+          name: 'Material Ui',
+          icon: 'SiMaterialUi',
+          color: {
+            hex: '#007dc5'
+          }
+        },
+        {
+          id: 'ckrzahxlci4yv0b93rv3yr97l',
+          name: 'Netlify',
+          icon: 'SiNetlify',
+          color: {
+            hex: '#008ab5'
+          }
+        },
+        {
+          id: 'ckrzx3fuwj3ui0b2983s4fvai',
+          name: 'Styled Components',
+          icon: 'SiStyledComponents',
+          color: {
+            hex: '#dd877a'
+          }
+        },
+        {
+          id: 'ckrykwcw8gtwi0b931k3oxb06',
+          name: 'JavaScript',
+          icon: 'SiJavascript',
+          color: {
+            hex: '#f7df1e'
+          }
+        },
+        {
+          id: 'cks2ulx88npb40c86fgc27581',
+          name: 'Auth0',
+          icon: 'SiAuth0',
+          color: {
+            hex: '#e45123'
+          }
+        }
+      ],
+      description:
+        'I have created this "NETFLIX-CLONE" using React.js, Auth0, Material-Ui and Redux-toolkit\n- User can login and signup .\n- User can see multiple rows of movies of different categories .\n- User can also watch the trailer of movie by clicking on it.\n- I have used axios to fetch all data of movies ',
+      repo: 'https://github.com/theamargupta/netflix-project-attainu',
+      live: 'https://amar-netflix.netlify.app/',
+      feature: true
+    },
+    {
+      id: 'ckrylfipkguz90b93u2q265ma',
+      title: "Amar's Ecommerce ",
+      tools: [
+        {
+          id: 'ckrykwcw8gtwi0b931k3oxb06',
+          name: 'JavaScript',
+          icon: 'SiJavascript',
+          color: {
+            hex: '#f7df1e'
+          }
+        },
+        {
+          id: 'ckrykxbm8gu1e0b29cgknd5yk',
+          name: 'React',
+          icon: 'SiReact',
+          color: {
+            hex: '#61dafb'
+          }
+        },
+        {
+          id: 'ckrylbhncguxp0b292ppigqbh',
+          name: 'Graphql',
+          icon: 'SiGraphql',
+          color: {
+            hex: '#de33a6'
+          }
+        },
+        {
+          id: 'ckrylcy48gv5w0c86udrabt17',
+          name: 'Heroku',
+          icon: 'SiHeroku',
+          color: {
+            hex: '#430098'
+          }
+        },
+        {
+          id: 'ckrzahxlci4yv0b93rv3yr97l',
+          name: 'Netlify',
+          icon: 'SiNetlify',
+          color: {
+            hex: '#008ab5'
+          }
+        }
+      ],
+      description:
+        'I made this ecommerce with backend which is deployed on Heroku and frontend(React) deployed on Netlify.\nMongoDB as database\n\nSuggest me things i can create on it to add on ?\n\nWebsite Link => https://lnkd.in/e-pFQYm\nCms Link => https://lnkd.in/ewk43FV',
+      repo: 'https://github.com/theamargupta/usaecommerce',
+      live: 'https://usa-ecommerce.netlify.app/',
+      feature: true
+    }
+  ]
+};
